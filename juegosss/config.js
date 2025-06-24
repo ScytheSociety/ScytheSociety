c3229f9b844cf69c1f3fc54354ca05f9ee012ad4
ScytheSociety <scythesociety@gmail.com>
2025-06-24 23:16:25 +0200

    Update config.js

--- juegosss/config.js
+++ juegosss/config.js
@@ -147,7 +147,7 @@ const GameConfig = {
   // ======================================================
 
   BOSS_CONFIG: {
-    health: 2000, // Vida del boss
+    health: 500, // Vida del boss
     size: 120, // Tamaño
     speed: 0.002, // Velocidad base
 
